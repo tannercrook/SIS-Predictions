4b3ce8d3689d404d34cdc4ec6503976eab59e7d9
tanner@crooktec.com <tanner@crooktec.com>
2018-10-05 02:43:11 -0600

    Working Model

--- CapstoneAnalysis.R
+++ CapstoneAnalysis.R
@@ -71,18 +71,34 @@ res.famd <- FAMD(noGradGrades, graph=FALSE)
 
 # Logistic Regression
 # ==============================================
+# Get the course grade data
+psGrades = read.csv("~/Documents/capData/PS_StoredGrades.csv", header=TRUE)
+icGrades = read.csv("~/Documents/capData/IC_StoredGrades.csv", header=TRUE)
+
+grades <- rbind(icGrades,psGrades)
+
 
 failedGrades <- grades[grades$score %in% c('F','D-','D','D+'), ]
+
+
+# Drop some columns
+grades$percentScore <- NULL
 failedGrades$gradYear <- NULL
 failedGrades$gradSchool <- NULL
 failedGrades$studentNumber <- NULL
+failedGrades$dateStored <- NULL
+failedGrades$percentScore <- NULL
+failedGrades$creditType <- NULL
+failedGrades$schedGradYear<- NULL
 
 summary(failedGrades)
 failedGrades <- failedGrades[complete.cases(failedGrades), ]
 
-failedGrades$dropout <- failedGrades$graduated
-failedGrades$dropout <- factor(failedGrades$dropout, levels=c(0,1), labels=c(1, 0))
-failedGrades$graduated <- NULL
+#failedGrades$dropout <- failedGrades$graduated
+#failedGrades$dropout <- factor(failedGrades$dropout, levels=c(0,1), labels=c(1, 0))
+#failedGrades$graduated <- NULL
+
+failedGrades <- failedGrades[failedGrades$schedGradYear >= 2010, ]
 
 
 library('caTools')
@@ -91,23 +107,23 @@ library('caTools')
 set.seed(111)
 
 #sample <- sample.split(tele$Churn, SplitRatio=0.70)
-train <- failedGrades[1:15000,]
-test <- failedGrades[15001:18410,]
+train <- failedGrades[1:12000,]
+test <- failedGrades[12001:18410,]
 
-model <- glm(dropout ~ ., family = binomial(link="logit"), data = train)
+model <- glm(graduated ~ ., family = binomial(link="logit"), data = train)
 summary(model)
 
 
-model3 <- glm(dropout ~ gradeLevel+courseName, family = binomial(link = "logit"), data=train)
+model3 <- glm(graduated ~ factor(courseName)+factor(score), family = binomial(link = "logit"), data=train)
 summary(model3)
 
 predict <- predict(model3, type="response")
 
 # Analyzing results of test
-table(train$dropout, predict > 0.5)
+table(train$graduated, predict > 0.5)
 library(ROCR)
 
-ROCRpred <- prediction(predict, train$dropout)
+ROCRpred <- prediction(predict, train$graduated)
 ROCRPerf <- performance(ROCRpred, 'tpr', 'fpr')
 plot(ROCRPerf, colorize = TRUE, text.adj = c(-0.2,1.7))
 
@@ -116,7 +132,16 @@ auc <- auc@y.values[[1]]
 auc
 
 
+modcoef <- summary(model3)[["coefficients"]]
+modcoef[order(modcoef[ , 4]), ]
+
+test <- failedGrades[failedGrades$courseName=="Biology", ]
+test$graduated <- factor(test$graduated, levels=c(0,1), labels=c("No", "Yes"))
+
+test <- failedGrades[failedGrades$courseName=="English I", ]
+test$graduated <- factor(test$graduated, levels=c(0,1), labels=c("No", "Yes"))
+summary(test)
 
 
-
+exp(cbind(Odd_Ratio = coef(model3), confint.default(model3)))
 
