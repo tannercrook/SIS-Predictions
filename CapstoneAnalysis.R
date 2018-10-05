4eeb386ee4814650963cd2a28ba65c961fb564f0
Tanner <tanner@crooktec.com>
2018-10-04 18:40:38 -0600

    Updated R file

--- CapstoneAnalysis.R
+++ CapstoneAnalysis.R
@@ -6,7 +6,7 @@ icDemographics = read.csv("~/Documents/capData/IC_Demographics.csv", header=TRUE
 
 # Delete some of the columns. They don't have the data between the two systems to support analysis
 psDemographics$IEP <- NULL
-psDemographics$FREEREDUCED <- NULL
+psDemographics$FreeReduced <- NULL
 
 summary(psDemographics)
 summary(icDemographics)
@@ -17,3 +17,18 @@ icGrades = read.csv("~/Documents/capData/IC_StoredGrades.csv", header=TRUE)
 
 summary(psGrades)
 summary(icGrades)
+
+
+# Combine the data
+demographics <- rbind(icDemographics,psDemographics)
+grades <- rbind(icGrades,psGrades)
+
+# Univariate Stats
+barplot(table(demographics$graduated),xlab="Graduated",ylab="Frequency")
+
+
+
+library(FactoMineR)
+library(factoextra)
+
+
