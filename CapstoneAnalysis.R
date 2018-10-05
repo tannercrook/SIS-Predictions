2e60a015de1282e6b60f6a26d6e78dcc640bcd36
tanner@crooktec.com <tanner@crooktec.com>
2018-10-04 22:09:48 -0600

    Added Logistic Regression

--- CapstoneAnalysis.R
+++ CapstoneAnalysis.R
@@ -23,6 +23,17 @@ summary(icGrades)
 demographics <- rbind(icDemographics,psDemographics)
 grades <- rbind(icGrades,psGrades)
 
+#transform binary to yes,no
+demographics$graduated <- factor(demographics$graduated, levels=c(0,1), labels=c("No", "Yes"))
+grades$graduated <- factor(grades$graduated, levels=c(0,1), labels=c("No", "Yes"))
+
+# Remove bad schedGradYear
+demographics<-demographics[demographics$schedGradYear != 0, ]
+
+# Drop some columns
+grades$percentScore <- NULL
+
+
 # Univariate Stats
 barplot(table(demographics$graduated),xlab="Graduated",ylab="Frequency")
 
@@ -30,5 +41,72 @@ barplot(table(demographics$graduated),xlab="Graduated",ylab="Frequency")
 
 library(FactoMineR)
 library(factoextra)
+
+# Find missing values
+sapply(grades, function(x) sum(is.na(x)))
+
+#currently won't run because the dataset is simply too large
+#res.famd <- FAMD(grades, graph=FALSE)
+
+
+# Lets get data on people who actually graduated
+gradDemo <- demographics[demographics$graduated == 1, ]
+
+# Get set of those who didn't
+noGradDemo <- demographics[demographics$graduated != 1, ]
+noGradDemo$gradYear <- NULL
+noGradDemo$gradSchool <- NULL
+
+noGradGrades <- grades[grades$graduated != 'Yes', ]
+noGradGrades <- noGradGrades[complete.cases(noGradGrades), ]
+
+summary(noGradGrades)
+res.famd <- FAMD(noGradGrades, graph=FALSE)
+
+
+
+
+
+
+
+# Logistic Regression
+# ==============================================
+
+failedGrades <- grades[grades$score %in% c('F','D-','D','D+'), ]
+failedGrades$gradYear <- NULL
+failedGrades$gradSchool <- NULL
+failedGrades$studentNumber <- NULL
+
+summary(failedGrades)
+failedGrades <- failedGrades[complete.cases(failedGrades), ]
+
+failedGrades$dropout <- failedGrades$graduated
+failedGrades$dropout <- factor(failedGrades$dropout, levels=c(0,1), labels=c(1, 0))
+failedGrades$graduated <- NULL
+
+
+library('caTools')
+
+# Set the seed so we can run multiple times
+set.seed(111)
+
+#sample <- sample.split(tele$Churn, SplitRatio=0.70)
+train <- failedGrades[1:10000,]
+test <- failedGrades[10001:18410,]
+
+model <- glm(dropout ~ ., family = binomial(link="logit"), data = train)
+summary(model)
+
+
+model3 <- glm(dropout ~ gradeLevel+courseName, family = binomial(link = "logit"), data=train)
+summary(model3)
+
+predict <- predict(model3, type="response")
+exp(cbind(Odd_Ratio = coef(model3), confint(model3)))
+
+
+
+
+
 
 
