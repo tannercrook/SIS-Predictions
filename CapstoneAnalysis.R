e9b9ee6856e3aa29922b5fb9ed55e239c6505c15
tanner@crooktec.com <tanner@crooktec.com>
2018-10-07 19:28:33 -0600

    Final Analysis

--- CapstoneAnalysis.R
+++ CapstoneAnalysis.R
@@ -71,18 +71,33 @@ res.famd <- FAMD(noGradGrades, graph=FALSE)
 
 # Logistic Regression
 # ==============================================
+# Get the course grade data
+psGrades = read.csv("~/Documents/capData/PS_StoredGrades.csv", header=TRUE)
+icGrades = read.csv("~/Documents/capData/IC_StoredGrades.csv", header=TRUE)
+
+grades <- rbind(icGrades,psGrades)
+
 
 failedGrades <- grades[grades$score %in% c('F','D-','D','D+'), ]
+
+
+# Drop some columns
+grades$percentScore <- NULL
 failedGrades$gradYear <- NULL
 failedGrades$gradSchool <- NULL
 failedGrades$studentNumber <- NULL
+failedGrades$dateStored <- NULL
+failedGrades$percentScore <- NULL
+failedGrades$creditType <- NULL
+failedGrades$schedGradYear<- NULL
 
 summary(failedGrades)
 failedGrades <- failedGrades[complete.cases(failedGrades), ]
 
-failedGrades$dropout <- failedGrades$graduated
-failedGrades$dropout <- factor(failedGrades$dropout, levels=c(0,1), labels=c(1, 0))
-failedGrades$graduated <- NULL
+#failedGrades$dropout <- failedGrades$graduated
+#failedGrades$dropout <- factor(failedGrades$dropout, levels=c(0,1), labels=c(1, 0))
+#failedGrades$graduated <- NULL
+
 
 
 library('caTools')
@@ -91,62 +106,15 @@ library('caTools')
 set.seed(111)
 
 #sample <- sample.split(tele$Churn, SplitRatio=0.70)
-train <- failedGrades[1:15000,]
-test <- failedGrades[15001:18410,]
+train <- failedGrades[1:12000,]
+test <- failedGrades[12001:18410,]
 
-model <- glm(dropout ~ ., family = binomial(link="logit"), data = train)
+model <- glm(graduated ~ ., family = binomial(link="logit"), data = train)
 summary(model)
 
 
-model3 <- glm(dropout ~ gradeLevel+courseName, family = binomial(link = "logit"), data=train)
+model3 <- glm(graduated ~ factor(courseName)+factor(score), family = binomial(link = "logit"), data=train)
 summary(model3)
-
-predict <- predict(model3, type="response")
-
-# Analyzing results of test
-table(train$dropout, predict > 0.5)
-library(ROCR)
-
-ROCRpred <- prediction(predict, train$dropout)
-ROCRPerf <- performance(ROCRpred, 'tpr', 'fpr')
-plot(ROCRPerf, colorize = TRUE, text.adj = c(-0.2,1.7))
-
-auc <- performance(ROCRpred, measure = "auc")
-auc <- auc@y.values[[1]]
-auc
-
-
-
-
-# Fail Count Dataset Analysis
-# =====================================
-
-failCount = read.csv("~/Documents/capData/failCount.csv", header=TRUE)
-failCount$studentNumber <- NULL
-failCount$Total <- NULL
-
-#failCount$dropout <- failCount$graduated
-#failCount$dropout <- factor(failCount$dropout, levels=c(0,1), labels=c(1, 0))
-#failCount$graduated <- NULL
-
-# change NAs to 0s
-failCount[is.na(failCount)] <- 0
-
-
-library('caTools')
-
-# Set the seed so we can run multiple times
-set.seed(111)
-
-#sample <- sample.split(tele$Churn, SplitRatio=0.70)
-train <- failCount[1:2000,]
-test <- failCount[2001:2594,]
-
-
-model3 <- glm(graduated ~ ., family = binomial(link = "logit"), data=train)
-summary(model3)
-
-model3 <- glm(dropout ~ C+CMinus+F, family = binomial(link = "logit"), data=train)
 
 predict <- predict(model3, type="response")
 
@@ -162,6 +130,128 @@ auc <- performance(ROCRpred, measure = "auc")
 auc <- auc@y.values[[1]]
 auc
 
+exp(cbind(Odd_Ratio = coef(model3), confint.default(model3)))
+odds <- exp(cbind(Odd_Ratio = coef(model3), confint.default(model3)))
+
+write.csv(odds, file = "~/Documents/capData/courseOdds.csv")
+
+
+
+
+
+
+
+
+
+# Fail Count Dataset Analysis
+# =====================================
+
+failCount = read.csv("~/Documents/capData/failCount.csv", header=TRUE)
+
+# remove technical data
+failCount$studentNumber <- NULL
+failCount$Total <- NULL
+
+# change NAs to 0s
+failCount[is.na(failCount)] <- 0
+
+# Make a function
+cat_counts <- function(A){
+  if (A >= 0 && A <= 6) {
+    return('0-6')
+  } else if (A > 6 && A <= 12) {
+    return('6-12')
+  } else if (A > 12 && A <= 24) {
+    return('12-24')
+  } else if (A > 24 && A <= 36) {
+    return('24-36')
+  } else if (A > 36 && A <= 48) {
+    return('36-48')
+  } else if (A > 48 && A <= 62) {
+    return('48-62')
+  } else if (A > 62) {
+    return('62+')
+  }
+}
+
+failCount$A_cat <- sapply(failCount$A, cat_counts)
+failCount$AMinus_cat <- sapply(failCount$AMinus, cat_counts)
+failCount$BPlus_cat <- sapply(failCount$BPlus, cat_counts)
+failCount$B_cat <- sapply(failCount$B, cat_counts)
+failCount$BMinus_cat <- sapply(failCount$BMinus, cat_counts)
+failCount$CPlus_cat <- sapply(failCount$CPlus, cat_counts)
+failCount$C_cat <- sapply(failCount$C, cat_counts)
+failCount$CMinus_cat <- sapply(failCount$CMinus, cat_counts)
+failCount$DPlus_cat <- sapply(failCount$DPlus, cat_counts)
+failCount$D_cat <- sapply(failCount$D, cat_counts)
+failCount$DMinus_cat <- sapply(failCount$DMinus, cat_counts)
+failCount$F_cat <- sapply(failCount$F, cat_counts)
+
+failCount$A_cat <- as.factor(failCount$A_cat)
+failCount$AMinus_cat <- as.factor(failCount$AMinus_cat)
+failCount$BPlus_cat <- as.factor(failCount$BPlus_cat)
+failCount$B_cat <- as.factor(failCount$B_cat)
+failCount$BMinus_cat <- as.factor(failCount$BMinus_cat)
+failCount$CPlus_cat <- as.factor(failCount$CPlus_cat)
+failCount$C_cat <- as.factor(failCount$C_cat)
+failCount$CMinus_cat <- as.factor(failCount$CMinus_cat)
+failCount$DPlus_cat <- as.factor(failCount$DPlus_cat)
+failCount$D_cat <- as.factor(failCount$D_cat)
+failCount$DMinus_cat <- as.factor(failCount$DMinus_cat)
+failCount$F_cat <- as.factor(failCount$F_cat)
+
+
+# Fail Count FAMD
+# ====================================================
+
+library(FactoMineR)
+library(factoextra)
+
+res.famd <- FAMD(failCount, graph=TRUE)
+
+fviz_mfa_ind(res.famd, habillage = "graduated" ,addEllipses = TRUE)
+
+fviz_famd_var(res.famd, col.var = "contrib", repel = TRUE,gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
+
+
+
+# LOGISTICAL REGRESSION
+# ======================================================
+
+library('caTools')
+
+# Set the seed so we can run multiple times
+set.seed(111)
+
+#sample <- sample.split(tele$Churn, SplitRatio=0.70)
+train <- failCount[1:2000,]
+test <- failCount[2001:2594,]
+
+
+model3 <- glm(graduated ~ ., family = binomial(link = "logit"), data=train)
+summary(model3)
+
+
+model3 <- glm(graduated ~ as.factor(F_cat)+as.factor(A_cat), family = binomial(link = "logit"), data=train)
+summary(model3)
+
+predict <- predict(model3, type="response")
+
+
+
+# Analyzing results of test
+table(train$graduated, predict > 0.5)
+library(ROCR)
+
+ROCRpred <- prediction(predict, train$graduated)
+ROCRPerf <- performance(ROCRpred, 'tpr', 'fpr')
+plot(ROCRPerf, colorize = TRUE, text.adj = c(-0.2,1.7))
+
+auc <- performance(ROCRpred, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
+
 
 exp(cbind(Odd_Ratio = coef(model3), confint(model3)))
+
 
